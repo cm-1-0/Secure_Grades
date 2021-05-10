7aa64954e18e34df6025494753d3d7abd3198b0a
cm-1-0 <54222924+cm-1-0@users.noreply.github.com>
2021-05-10 18:28:29 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -8,16 +8,28 @@ if ques_1 == "Soccer10":
     ques_2 = input("How many grades would you like to enter: ")
     for num in ques_2:
         grade = input("Enter the grades: ")
-    eng_file.write(grade)
-    eng_file.close()
+        eng_file.write(grade)
+        eng_file.close()
 
-    eng_file = open("English Grade", "r")
-    line = eng_file.read()
-    line.split(", ")
-    total = sum([int(num) for num in line.split(", ")])
-    avg = total/int(ques_2)
-    print(avg)
+# eng_file = open("English Grade", "r")
+# line = eng_file.read()
+# line.split(", ")
+# total = sum([int(num) for num in line.split(", ")])
+# avg = total / int(ques_2)
+# print(avg)
 
 key = Fernet.generate_key()
-key_file = open("key", "wb")
-key_file.write(key)
+with open("key", "wb") as key_file:
+    key_file.write(key)
+
+
+def load_key():
+    return open("key", "rb").read()
+
+
+fernet = Fernet(key)
+with open("English Grade", "rb") as file:
+    original = file.read()
+encrypted = fernet.encrypt(original)
+with open("English Grade", "wb") as encrypted_file:
+    encrypted_file.write(encrypted)
